59518c60709fb15d78e47d6079d44d1be3c0ea1c
Jaclyn <jaclynnbarrera@gmail.com>
2021-01-21 23:17:15 -0500

    commit tree 9c33d8485dbb01ab8197bd1748722d46028e1c17

--- lib/person.rb
+++ lib/person.rb
@@ -1,97 +1,86 @@
-# your code goes here
 
 class Person
     attr_reader :name
     attr_accessor :bank_account, :happiness, :hygiene
-    
-    def initialize(name_arg)
-        @name = name_arg
+
+    def initialize(name)
+        @name = name
         @bank_account = 25
         @happiness = 8
         @hygiene = 8
-    end 
+    end
 
     def happiness
-        if @happiness <  0
+        if @happiness > 10
+           @happiness = 10
+        elsif @happiness < 0
             @happiness = 0
-        elsif @happiness > 10
-            @happiness = 10 
-        else 
+        else
             @happiness
         end 
-    end 
+    end
 
-    def hygiene
-        if @hygiene <  0
-            @hygiene = 0
-        elsif @hygiene > 10
-            @hygiene = 10 
-        else 
-            @hygiene
-        end 
-    end 
+    def hygiene=(num)
+        @hygiene = num
+        if @hygiene > 10
+            @hygiene = 10
+         elsif @hygiene < 0
+             @hygiene = 0
+         else
+             @hygiene
+         end 
+    end
 
     def happy?
         if @happiness > 7
             true
-        else 
+        else
             false
-        end 
-    end 
+        end
+    end
 
     def clean?
         if @hygiene > 7
             true
-        else 
+        else
             false
-        end 
-    end 
+        end
+    end
 
     def get_paid(salary)
         @bank_account += salary
-        return 'all about the benjamins'
-    end 
+        @bank_account
+        "all about the benjamins"
+    end
 
     def take_bath
         self.hygiene += 4
-        return "♪ Rub-a-dub just relaxing in the tub ♫"
-    end 
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
 
     def work_out
         self.hygiene -= 3
         self.happiness += 2
-            if @happiness > 10
-                @happiness = 10
-            end 
-        "♪ another one bites the dust ♫"   
-    end 
+        "♪ another one bites the dust ♫"
+    end
 
-    def call_friend(friend)
-        self.happiness += 3 
-        friend.happiness += 3 
-        return "Hi #{friend.name}! It's #{self.name}. How are you?"
-    end 
+    def call_friend(caller)
+        self.happiness += 3
+        caller.happiness += 3
+        "Hi #{caller.name}! It's #{self.name}. How are you?"
+    end
 
-    def start_conversation (friend, topic)
+    def start_conversation(person,topic)
         if topic == "politics"
+            person.happiness -= 2
             self.happiness -= 2
-            friend.happiness -= 2
-            return "blah blah partisan blah lobbyist"
-        end 
-
-        if topic == "weather"
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            person.happiness += 1
             self.happiness += 1
-            friend.happiness += 1 
-            return "blah blah sun blah rain"
-        end 
-
-        if topic != "weather" or "politics"
-            return "blah blah blah blah blah"
-        end 
-    end 
+            "blah blah sun blah rain"
+        else 
+            "blah blah blah blah blah"
+        end
+    end
 end
-
-
-# - **attr_reader gives a getter method
-# - **attr_writer sets up a setter method
-# - **attr_accessor ives us both a getter and a setter
